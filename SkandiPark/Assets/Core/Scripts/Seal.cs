2a3ad1f27ff66a9bef6cf76aa5758ee03dad7d46
David Gudmund <dada5@student.eadania.dk>
2025-09-15 14:12:42 +0200

    Update
    Har arbejdet med selve seal scriptet og gameplay, men er ikke færdig. Skal arbejde videre med det.

--- SkandiPark/Assets/Core/Scripts/Seal.cs
+++ SkandiPark/Assets/Core/Scripts/Seal.cs
@@ -2,15 +2,52 @@ using System.Collections;
 using System.Runtime.CompilerServices;
 using UnityEngine;
 
+public enum SpawnType
+{
+    Standard,
+    Polarbear
+}
+
 public class Seal : MonoBehaviour
 {
+    [Header("Components")]
+    //Her kan jeg tilføje PlayerInputController components samt andre hvis nødvendigt.
+
+    [Header("Graphics")]
+    [SerializeField] private Sprite sealStandard;
+    [SerializeField] private Sprite sealStandardHit;
+    [SerializeField] private Sprite polarbear;
+    [SerializeField] private Sprite polarBearHit;
+
     [Header("Position")]
     [SerializeField] private Vector2 startPosition;
     [SerializeField] private Vector2 endPosition;
 
-    [Header("Visable")]
+    [Header("Visability")]
     [SerializeField] private float showDuration;
     [SerializeField] private float duration;
+
+    [Header("GameStats")]
+    [SerializeField] private float sealStandardSpawnRate;
+    [SerializeField] private float polarbearSpawnRate;
+
+    private SpriteRenderer _spriteRenderer;
+
+    //Seal parametre
+    private bool hittable = true;
+    private SpawnType spawnType;
+
+    private void Awake()
+    {
+        //References
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Start()
+    {
+        CreateNext();
+        StartCoroutine(ShowHide(startPosition, endPosition));
+    }
 
     private IEnumerator ShowHide(Vector2 start, Vector2 end)
     {
@@ -48,8 +85,25 @@ public class Seal : MonoBehaviour
         transform.localPosition = start;
     }
 
-    private void Start()
+    private void CreateNext()
     {
-        StartCoroutine(ShowHide(startPosition, endPosition));
+        float random = Random.Range(0f, 1f);
+
+        if(random < polarbearSpawnRate)
+        {
+            spawnType = SpawnType.Polarbear;
+        }
+        else
+        {
+            random = Random.Range(0f, 1f);
+
+            if (random < sealStandardSpawnRate)
+            {
+                spawnType = SpawnType.Standard;
+                _spriteRenderer.sprite = sealStandard;
+            }
+        }
+
+        hittable = true;
     }
 }
