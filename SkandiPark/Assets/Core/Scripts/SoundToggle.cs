d403187994000b5a102f933a5ef3ea95820fd4c9
David Gudmund <dada5@student.eadania.dk>
2025-09-25 20:34:29 +0200

    GraphicsBank
    Fikset med soundoff icon

--- SkandiPark/Assets/Core/Scripts/SoundToggle.cs
+++ SkandiPark/Assets/Core/Scripts/SoundToggle.cs
@@ -1,33 +1,106 @@
 using UnityEngine;
 using UnityEngine.UI;
+public enum IconSet { PauseMenu, SettingsMenu }
 
 public class SoundToggle : MonoBehaviour
 {
-    public Sprite soundOnIcon; //Billedet af lyden er slået til.
-    public Sprite soundOffIcon; //Billedet af lyden er slået fra.
-    public Image buttonImage;
+    [Header("References")]
+    [SerializeField] private GraphicsBank bank;
+    [SerializeField] private IconSet iconSet = IconSet.PauseMenu;
 
+    private Image buttonImage;
+    private Sprite soundOnIcon;
+    private Sprite soundOffIcon;
     private bool isSoundOn = true;
 
-    void Start()
+    private void Awake()
     {
-        //Vi sørger for at det starter ud korrekt.
-        UpdateIcon();
+        if(buttonImage == null)
+        {
+            buttonImage = GetComponent<Image>();
+        }
+
+        PullIconsFromBank();
+        ApplyIcon();
+    }
+
+    private void OnEnable()
+    {
+        #if UNITY_EDITOR
+        GraphicsBank.Changed += OnBankChanged;  // live-opdater i editor
+        #endif
+
+        // sync lydsystemet
         AudioManager.Instance?.SetMuted(!isSoundOn);
+        ApplyIcon();
+    }
+
+    void OnDisable()
+    {
+        #if UNITY_EDITOR
+        GraphicsBank.Changed -= OnBankChanged;
+        #endif
     }
 
     public void ToggleSound()
     {
         isSoundOn = !isSoundOn;
         AudioManager.Instance?.SetMuted(!isSoundOn);
-        UpdateIcon();
+        ApplyIcon();
     }
 
-    void UpdateIcon()
+    private void ApplyIcon()
     {
-        if (isSoundOn)
-            buttonImage.sprite = soundOnIcon;
-        else
-            buttonImage.sprite = soundOffIcon;
+        if (buttonImage == null)
+        {
+            return;
+        }
+        buttonImage.sprite = isSoundOn ? soundOnIcon : soundOffIcon;
     }
+
+    private void PullIconsFromBank()
+    {
+        if (bank == null)
+        {
+            return;
+        }
+
+        switch (iconSet)
+        {
+            case IconSet.PauseMenu:
+                soundOnIcon = bank.pauseSoundButton;
+                soundOffIcon = bank.pauseSoundButtonOff;
+                break;
+
+            case IconSet.SettingsMenu:
+                soundOnIcon = bank.settingsSoundButton;
+                soundOffIcon = bank.settingsSoundButtonOff;
+                break;
+        }
+    }
+
+    #if UNITY_EDITOR
+    private void OnBankChanged(GraphicsBank b)
+    {
+        if (b != bank)
+        {
+            return;
+        }
+
+        PullIconsFromBank();
+        ApplyIcon();
+    }
+
+    private void OnValidate()
+    {
+        // hjælper når du skifter IconSet i Inspector
+        if (buttonImage == null)
+        {
+            buttonImage = GetComponent<Image>();
+        }
+
+        PullIconsFromBank();
+        ApplyIcon();
+    }
+    #endif
 }
